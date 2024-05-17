c952ce724f37fba376afc2665601bd9d375ee185
jackhein <moustiquejunior@gmail.com>
2024-05-17 18:59:25 +0200

    Update of DBC files through SQL commands

---
+++ data/sql/db-world/base/event_hallow_s_end_60_item_dbc.sql
@@ -0,0 +1,15 @@
+-- The Horseman's Notched Blade
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60001,2,4,-1,1,31419,21,3);
+-- The Horseman's Headless Helm
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60002, 4, 7, -1, 8, 51920, 1, 0);
+-- The Horseman's Stamp Ring
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60003, 4, 1, -1, 3, 47325, 11, 0);
+-- Apprentice Witch Band
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60004, 4, 1, -1, 3, 47325, 11, 0);
+-- Ring of Ghoulish Joy
+INSERT IGNORE INTO item_dbc (`ID`, `ClassID`, `SubclassID`, `Sound_Override_Subclassid`, `Material`, `DisplayInfoID`, `InventoryType`, `SheatheType`)
+ VALUES (60005, 4, 1, -1, 3, 47325, 11, 0);
